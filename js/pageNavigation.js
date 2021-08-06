9e7d30099d5860b7605f9b7fb45c7a9ad2ab28f2
TechWorldInc <76709090+TechWorldInc@users.noreply.github.com>
2021-08-06 11:01:31 +0200

    update PageNavigation

--- js/pageNavigation.js
+++ js/pageNavigation.js
@@ -1,21 +1,21 @@
 function navigateTo(url, fromHome = true) {
     if (fromHome) {
         var obj = document.getElementById("mainLogo");
-        obj.style.transition = "margin-top 1.0s, opacity 0.75s";
+        obj.style.transition = "margin-top 0.5s, opacity 0.75s";
         obj.style.opacity = "1";
         setTimeout(() => {
             obj.style.transition = "";
             obj.style.opacity = "0";
         }, 100);
         var textNode = document.getElementById("mainLogoAltText");
-        textNode.style.transition = "margin-top 1.0s, opacity 0.75s";
+        textNode.style.transition = "margin-top 0.5s, opacity 0.75s";
         textNode.style.opacity = "1";
         setTimeout(() => {
             textNode.style.transition = "";
             textNode.style.opacity = "0";
         }, 500);
         var textNode1 = document.getElementById("navText");
-        textNode1.style.transition = "margin-top 1.0s, opacity 0.75s";
+        textNode1.style.transition = "margin-top 0.5s, opacity 0.75s";
         textNode1.style.opacity = "1";
         setTimeout(() => {
             textNode1.style.transition = "";
