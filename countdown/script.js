e0cf7adaae52cf664bc7f6ff10aced4d8ba6352f
Izaak Kuipers <76709090+TechWorldInc@users.noreply.github.com>
2021-10-27 11:08:10 +0200

    Update

--- countdown/script.js
+++ countdown/script.js
@@ -27,24 +27,28 @@ onload = function() {
         let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        dayOut.innerText = days.toString().padStart(3, "0");
-        hourOut.innerText = hours.toString().padStart(2, "0");
-        minOut.innerText = minutes.toString().padStart(2, "0");
-        secOut.innerText = seconds.toString().padStart(2, "0");
-        wwkOut.innerText = weeks.toString().padStart(2, "0");
+        
         cddOut.innerText = rawCountDownDate;
-        mscOut.innerText = (countDownDate - Today).toString().padStart(11,"0");
 
-        let colons = document.getElementsByClassName("colon");
-
-        for (let i = 0; i < colons.length; i++) {
-            colons[i].classList.toggle("halfTransparent");
+        if (countDownDate - Today > 0) {
+            dayOut.innerText = days.toString().padStart(3, "0");
+            hourOut.innerText = hours.toString().padStart(2, "0");
+            minOut.innerText = minutes.toString().padStart(2, "0");
+            secOut.innerText = seconds.toString().padStart(2, "0");
+            wwkOut.innerText = weeks.toString().padStart(2, "0");
+            
+            mscOut.innerText = (countDownDate - Today).toString().padStart(11,"0");
+    
+            let colons = document.getElementsByClassName("colon");
+    
+            for (let i = 0; i < colons.length; i++) {
+                colons[i].classList.toggle("halfTransparent");
+            }
+        } else {
+            this.document.getElementById("countdown").style.display = "none";
+            this.document.getElementById("countdown-done").style.display = "";
+            confetti.start();
+            this.document.body.style.backgroundColor = "#034"
         }
     }, 500);
-}
-
-function toggleEditor() {
-    document.getElementById("currentDateSpan").classList.toggle("noDisplay");
-    document.getElementById("newDateInputSpan").classList.toggle("noDisplay");
 }
