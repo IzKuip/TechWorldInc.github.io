71847635a98b65081709831e4a29021037198f67
TWI <kuipers.izaak@2college.nl>
2021-11-07 21:12:49 +0100

    Update Countdown

--- countdown/script.js
+++ countdown/script.js
@@ -14,41 +14,27 @@ onload = function() {
     hourOut = document.getElementById("hourOut");
     minOut = document.getElementById("minOut");
     secOut = document.getElementById("secOut");
-    wwkOut = document.getElementById("weekOut");
-    cddOut = document.getElementById("dateOut");
-    mscOut = document.getElementById("secondsOut");
+
     setInterval(() => {
         const Today = new Date()
         let distance = countDownDate - Today;
 
         // Time calculations for days, hours, minutes and seconds
-        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        let weeks = Math.floor(days / 7) + 1;
-        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
-        cddOut.innerText = rawCountDownDate;
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24)).toString().padStart(3,"0");
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2,"0");
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2,"0");
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000).toString().padStart(2,"0");
 
         if (countDownDate - Today > 0) {
-            dayOut.innerText = days.toString().padStart(3, "0");
-            hourOut.innerText = hours.toString().padStart(2, "0");
-            minOut.innerText = minutes.toString().padStart(2, "0");
-            secOut.innerText = seconds.toString().padStart(2, "0");
-            wwkOut.innerText = weeks.toString().padStart(2, "0");
-            
-            mscOut.innerText = (countDownDate - Today).toString().padStart(11,"0");
-    
-            let colons = document.getElementsByClassName("colon");
-    
-            for (let i = 0; i < colons.length; i++) {
-                colons[i].classList.toggle("halfTransparent");
-            }
+            this.document.getElementById("out").innerText = `${days} : ${hours} : ${minutes} : ${seconds}`
+            this.document.getElementById("bg").innerText = `888 : 88 : 88 : 88`
         } else {
             this.document.getElementById("countdown").style.display = "none";
             this.document.getElementById("countdown-done").style.display = "";
             confetti.start();
-            this.document.body.style.backgroundColor = "#034"
+            this.document.body.style.backgroundColor = "#034";
+            this.document.body.style.color = "#0f0";
+            this.document.body.style.fontFamily = "monospace"
         }
     }, 500);
 }
